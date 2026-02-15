981f7b3757b7fec82ae2b9e62fa45b4771fa549d
Supraja <suprajarontala20@gmail.com>
2026-02-15 11:31:18 -0600

    final

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -17,10 +17,10 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={require("../../Assets/cust.png")}
+              imgPath={require("../../Assets/streaming.png")}
               isBlog={false}
-              title="Customer Contact Volume Forecasting with Real – Time Analytics"
-              description="Developed predictive models using regression, clustering, and time-series techniques. Built real-time pipelines with PySpark and AWS services. Integrated forecasts into Tableau dashboards to support operational planning."
+              title="Streaming Data Analytics with Automated ETL Pipeline"
+              description="Built an end-to-end analytics solution inspired by a streaming platform. Designed dimensional warehouse schema, developed Python-based ETL pipelines for data transformation, and executed KPI analysis using SQL. Integrated insights into Power BI dashboards to monitor engagement, churn, and genre performance."
             />
           </Col>
 
@@ -35,19 +35,19 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={require("../../Assets/chat.png")}
+              imgPath={require("../../Assets/age.png.jpeg")}
               isBlog={false}
-              title="Chatbot for Blind using Python"
-              description="Developed a voice-enabled chatbot to assist visually impaired users with daily tasks like playing music, telling time, and delivering news. Implemented speech recognition, text-to-speech, and API integrations for Wikipedia, weather, and news updates to ensure hands-free accessibility."
+              title="Age & Gender Prediction Application"
+              description="Developed a full-stack deep learning web application that predicts age and gender from facial images in real-time. Built a custom convolutional neural network by fine tuning ResNet50 with dual classification heads one for gender and one for age group. Designed a RESTful API backend using FastAPI with PyTorch inference.Created a responsive React frontend with Vite featuring live webcam capture with bounding box overlays and image upload functionality."
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={require("../../Assets/streaming.png")}
+              imgPath={require("../../Assets/chat.png")}
               isBlog={false}
-              title="Streaming Data Analytics with Automated ETL Pipeline"
-              description="Built an end-to-end analytics solution inspired by a streaming platform. Designed dimensional warehouse schema, developed Python-based ETL pipelines for data transformation, and executed KPI analysis using SQL. Integrated insights into Power BI dashboards to monitor engagement, churn, and genre performance."
+              title="Chatbot for Blind using Python"
+              description="Developed a voice-enabled chatbot to assist visually impaired users with daily tasks like playing music, telling time, and delivering news. Implemented speech recognition, text-to-speech, and API integrations for Wikipedia, weather, and news updates to ensure hands-free accessibility."
             />
           </Col>
         </Row>
